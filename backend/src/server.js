775f57b1838393cacc20b4115f2e00d33da99873
Pushpa yadav <pushpayadav50571@gmail.com>
2026-06-10 13:08:05 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
+//import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
 const app = express();
+//const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); //req.body
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api", messageRoutes);
 
-app.listen(3000, ()=>console.log(`Server is running on port ${PORT}..`));
+//make ready for deployment
+// if(process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+//     app.get("*", (_, res) => {
+//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+//     })
+// } 
+
+app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}..`));
+connectDB();
 
