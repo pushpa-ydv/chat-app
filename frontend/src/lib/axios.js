2447d8ffa43cf161ee46844ff440015840dd71eb
Pushpa yadav <pushpayadav50571@gmail.com>
2026-08-25 01:06:54 +0530

    Fix frontend typo

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VOTE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
